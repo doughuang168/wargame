1db992bf1d5cb292d92e43b751d7e00bb59baf27
Doug Huang <doug.huang@gmail.com>
2016-06-15 21:43:02 -0700

    add args handling

--- src/main/java/com/doughuang/WarGame.java
+++ src/main/java/com/doughuang/WarGame.java
@@ -5,8 +5,18 @@ import java.io.IOException;
 public class WarGame {
 
     public static void main( String[] args ) throws IOException {
+	//War wargame = new War();
+        //wargame.play(4,13,2);
         War wargame = new War();
-        wargame.play(4,13,2);
+        if (args.length == 3) {
+            int numberOfSuits = Integer.parseInt(args[0]);
+            int numberOfRanks = Integer.parseInt(args[1]);
+            int numberOfPlayers = Integer.parseInt(args[2]);
+            wargame.play(numberOfSuits,numberOfRanks,numberOfPlayers);
+        } else {
+            wargame.play(4,13,2);
+        }
+
 
     }
 
